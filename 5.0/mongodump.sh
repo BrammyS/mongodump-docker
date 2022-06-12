4cdf6f8950635f6915d4e38a0c46766a42a0ea90
Bram <bram.esendam@gmail.com>
2022-06-12 21:47:01 +0200

    Added default mongdb uri

--- 5.0/mongodump.sh
+++ 5.0/mongodump.sh
@@ -5,7 +5,7 @@ TIME=`/bin/date +%d-%m-%Y_%H-%M`
 DEST="/backups"
 
 # Dump from mongodb host into backup directory
-/usr/bin/mongodump --uri "$MONGO_URI" --gzip --archive=$DEST/$TIME
+/usr/bin/mongodump --uri "${MONGO_URI:-"mongodb://mongo:27017"}" --gzip --archive=$DEST/$TIME
 
 if [[ -z "${DELETE_AFTER}" ]]; then
     echo "DELETE_AFTER was not set. Not deleting old backups"
