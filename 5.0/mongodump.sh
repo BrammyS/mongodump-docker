b552c12bb97e79b81e8ee373f76675774f74695a
Bram <bram.esendam@gmail.com>
2022-06-12 20:47:00 +0200

    Added DELETE_AFTER

--- 5.0/mongodump.sh
+++ 5.0/mongodump.sh
@@ -1,9 +1,15 @@
 #!/bin/ash
+echo "Starting mongodump"
 
-# date/time
 TIME=`/bin/date +%d-%m-%Y_%H-%M`
-
-echo "$TIME Starting mongodump"
+DEST="/backups"
 
 # Dump from mongodb host into backup directory
-/usr/bin/mongodump --uri "$MONGO_URI" --gzip --archive=/backups/$TIME
+/usr/bin/mongodump --uri "$MONGO_URI" --gzip --archive=$DEST/$TIME
+
+if [[ -z "${DELETE_AFTER}" ]]; then
+    echo "DELETE_AFTER was not set. Not deleting old backups."
+else
+    # Delete backups older then X amount of days
+    find $DEST -type f -ctime +$DELETE_AFTER -exec rm -rf {} \;
+fi
