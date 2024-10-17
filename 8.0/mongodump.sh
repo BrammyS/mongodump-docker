25c866ca5050f0ccd91f163200c51c69c80e4927
Bram <bram.esendam@gmail.com>
2024-10-17 20:13:56 +0200

    Added support for mongodb 8

---
+++ 8.0/mongodump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TIME=`/bin/date +%d-%m-%Y_%H-%M`
+DEST="/backups"
+
+echo "Starting mongodump task - $TIME"
+
+# Dump from mongodb host into backup directory
+/usr/bin/mongodump --uri "${MONGO_URI:-"mongodb://mongo:27017"}" --gzip --archive=$DEST/$TIME
+
+if [[ -z "${DELETE_AFTER}" ]]; then
+    echo "DELETE_AFTER was not set. Not deleting old backups"
+else
+    echo "Deleting backups older then $DELETE_AFTER days"
+    find $DEST -type f -ctime +$DELETE_AFTER -exec rm -rf {} \;
+fi
